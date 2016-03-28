6ad925700ba9b45bb90fc08c64ccbac5fb1e3fbb
Hannah Krager <hannahkrager@gmail.com>
2016-03-28 15:29:09 -0700

    import error

--- src/clock.py
+++ src/clock.py
@@ -4,7 +4,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import lib2to3
 import requests
 
-from .api_call import get_one_sol
+from api_call import get_one_sol
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=5)
