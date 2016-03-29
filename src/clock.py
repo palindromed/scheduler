8aa11d789ad70c91215508b73c4c59edcd3bb785
Hannah Krager <hannahkrager@gmail.com>
2016-03-29 08:22:59 -0700

    start working with listeners for events

--- src/clock.py
+++ src/clock.py
@@ -1,14 +1,26 @@
 from __future__ import unicode_literals
 from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.events import (EVENT_JOB_EXECUTED, EVENT_JOB_ERROR,
+                                EVENT_JOB_MISSED)
 from worker import main
 
 
-sched = BlockingScheduler()
+scheduler = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=5)
+@scheduler.scheduled_job('interval', minutes=5)
 def timed_job():
     main()
 
 
-sched.start()
+scheduler.start()
+
+
+def my_listener(event):
+    if event.exception:
+        print('The job crashed :(', event)
+    else:
+        print('The job worked :)')
+
+scheduler.add_listener(my_listener, EVENT_JOB_EXECUTED |
+                       EVENT_JOB_ERROR | EVENT_JOB_MISSED)
