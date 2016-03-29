2db72df85eecff698bf343c82774adb5edd0c8c8
Hannah Krager <hannahkrager@gmail.com>
2016-03-28 20:30:10 -0700

    change email to mailgun

--- src/worker.py
+++ src/worker.py
@@ -2,7 +2,9 @@ from __future__ import unicode_literals, print_function
 import os
 from api_call import get_one_sol
 import redis
-import smtplib
+# import smtplib
+# from email.mime.text import MIMEText
+import requests
 
 
 def main():
@@ -10,40 +12,46 @@ def main():
     if redis_url is not None:
         red = redis.from_url(redis_url)
         new = os.getenv("NEW_REDIS")
-        if new:
-            sol = 0
-            red.set('SOL', sol)
-        else:
-            sol = int(red.get('SOL'))
+    else:
+        params = {
+            "subject": "Redis Error",
+            "text": "Could not connect to Redis"
+        }
+        send_mail(params)
+        return
+    if new:
+        sol = 0
+        red.set('SOL', sol)
+        # os.environ['new'] = 'False'
+    else:
+        sol = int(red.get('SOL'))
+    try:
         get_one_sol('curiosity', sol)
         sol += 1
         red.set('SOL', sol)
-        send_mail(sol)
+        params = {
+            "subject": 'Success!!',
+            "text": "All went well in API call. SOL for call was {}".format(sol)
+        }
+        send_mail(params)
         print(sol)
+    except requests.exceptions.HTTPError:
+        params = {
+            "subject": "API Error",
+            "text": "There was a problem connecting to the API."
+        }
+        send_mail(params)
 
 
-def send_mail(sol):
-    username = os.getenv('GMAIL_USERNAME')
-    password = os.getenv('GMAIL_PASSWORD')
+def send_mail(**kwargs):
+    return requests.post(
+        "https://api.mailgun.net/v3/sandbox683f8d129b354362b092d1be8762ae7e.mailgun.org",
+        auth=("api", os.environ.get("MAILGUN_API_KEY")),
+        data={"from": "Mars Rover Bot",
+              "to": ["Hannah", "hannahkrager@gmail.com"],
+              "subject": "{subject}".format(),
+              "text": "{text}".format()})
 
-    sender = 'cummins.hannah@gmail.com'
-    receivers = ['cummins.hannah@gmail.com']
-
-    message = """From: Me <cummins.hannah@gmail.com>
-    To: Hannah <cummins.hannah@gmail.com>
-    Subject: SMTP e-mail test
-
-    The script has run. SOL is at: {}""".format(sol)
-
-    try:
-        session = smtplib.SMTP('smtp.gmail.com', 587)
-        session.ehlo()
-        session.starttls()
-        session.login(username, password)
-        session.sendmail(sender, receivers, message)
-        print("Successfully sent email")
-    except smtplib.SMTPException:
-        print("Error: unable to send email")
 
 if __name__ == '__main__':
     main()
