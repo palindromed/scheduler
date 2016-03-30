0b9a82b06c9bfa7b79f03feb67ff85d1c9568ce1
Hannah Krager <hannahkrager@gmail.com>
2016-03-30 07:46:22 -0700

    db conn needed?

--- src/clock.py
+++ src/clock.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 from apscheduler.schedulers.blocking import BlockingScheduler
-from apscheduler.events import (EVENT_JOB_EXECUTED, EVENT_JOB_ERROR,
-                                EVENT_JOB_MISSED)
+
 from worker import main
 
 scheduler = BlockingScheduler()
@@ -12,13 +11,4 @@ def timed_job():
     main('curiosity')
 
 
-
-def my_listener(event):
-    if event.exception:
-        print('The job crashed :(', event)
-    else:
-        print('The job worked :)')
-
-scheduler.add_listener(timed_job, EVENT_JOB_EXECUTED |
-                       EVENT_JOB_ERROR | EVENT_JOB_MISSED)
 scheduler.start()
