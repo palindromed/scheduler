6e26528baf3c3500b3e70e13819b442a83d2b36e
Hannah Krager <hannahkrager@gmail.com>
2016-03-30 20:09:49 -0700

    why

--- src/api_call.py
+++ src/api_call.py
@@ -67,11 +67,13 @@ def populate_from_data(results):
     """Push the given list of photo dictionaries into the database."""
     photo_list = [Photo(**result) for result in results]
     print('photo_list made: {}'.format(len(photo_list)))
-    with transaction.manager:
-        DBSession.add_all(photo_list)
-        DBSession.flush()
-        transaction.commit()
-    print('Put to Database')
+    posts = DBSession.query(Rover).all()
+    print(posts)
+    # with transaction.manager:
+    #     DBSession.add_all(photo_list)
+    #     DBSession.flush()
+    #     transaction.commit()
+    # print('Put to Database')
 
 
 def get_one_sol(rover, sol, page):
