9b2954e67077815dba7698d89e07dd32d10a05bb
Hannah Krager <hannahkrager@gmail.com>
2016-03-30 19:34:54 -0700

    some commits then crash

--- src/api_call.py
+++ src/api_call.py
@@ -70,7 +70,6 @@ def populate_from_data(results):
     with transaction.manager:
         DBSession.add_all(photo_list)
         DBSession.flush()
-        transaction.commit()
     print('Put to Database')
 
 
