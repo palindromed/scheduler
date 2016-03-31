7f5c32efe8157c75c6527ceda7b0d876125f9813
Hannah Krager <hannahkrager@gmail.com>
2016-03-31 11:19:34 -0700

    change spelling

--- src/clock.py
+++ src/clock.py
@@ -18,7 +18,7 @@ scheduler = BlockingScheduler()
 
 @scheduler.scheduled_job('interval', minutes=2)
 def timed_job():
-    return_value = main('opportunity')
+    return_value = main('Opportunity')
     print(return_value)
 
 
