d215238225572a8c2f3329ba2e66cf3c4cc23538
Hannah Krager <hannahkrager@gmail.com>
2016-03-30 18:52:07 -0700

    check logic

--- src/api_call.py
+++ src/api_call.py
@@ -47,6 +47,7 @@ def fetch_photo_data(rover, sol, page):
 def populate_from_data(results):
     """Push the given list of photo dictionaries into the database."""
     photo_list = [Photo(**result) for result in results]
+    print('photo_list made: {}'.format(len(photo_list)))
     with transaction.manager:
         DBSession.add_all(photo_list)
         DBSession.flush()
