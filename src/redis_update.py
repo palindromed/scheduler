8a8fe81b2ba63dd5e1dc5275db5c63892822bdcc
Hannah Krager <hannahkrager@gmail.com>
2016-03-31 17:12:42 -0700

    switch to spirit

--- src/redis_update.py
+++ src/redis_update.py
@@ -5,7 +5,7 @@ import redis
 def redis_init():
     redis_url = os.getenv('REDISTOGO_URL', None)
     red = redis.from_url(redis_url)
-    red.set('SOL', 4)
+    red.set('SOL', 0)
     # red.set('PAGE', 3)
 
 if __name__ == "__main__":
