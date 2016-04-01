ad1801e6526d18d996c958282eca08635002c3dd
Hannah Krager <hannahkrager@gmail.com>
2016-03-31 17:13:20 -0700

    update sol for spirit

--- src/redis_update.py
+++ src/redis_update.py
@@ -7,6 +7,9 @@ def redis_init():
     red = redis.from_url(redis_url)
     red.set('SOL', 0)
     # red.set('PAGE', 3)
+    # opportunity = 75
+    # curiosity = 51
+    # spirit = 0
 
 if __name__ == "__main__":
     redis_init()
