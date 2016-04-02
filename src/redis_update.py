697753e3958f244042076225ed63ce798a8fa553
Hannah Krager <hannahkrager@gmail.com>
2016-04-01 17:56:08 -0700

    rotate rover

--- src/redis_update.py
+++ src/redis_update.py
@@ -5,9 +5,8 @@ import redis
 def redis_init():
     redis_url = os.getenv('REDISTOGO_URL', None)
     red = redis.from_url(redis_url)
-    red.set('SOL', 75)
-    # red.set('PAGE', 3)
-    # opportunity = 75
+    red.set('SOL', 164)
+    # opportunity = 404
     # curiosity = 295
     # spirit = 164
 
